1bca388dfc2a09c9d46d2428cdef0fade69f9d0a
AzizbekGayratov <azizbekgairatov@gmail.com>
2024-04-26 21:16:12 +0500

    a

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const box = document.querySelector('.product__content');
 const input = document.querySelector('.header__input');
+const search_result = document.querySelector('.search__result');
+const search__card = document.querySelector('.search__card')
 
 const render = (data) => {
-    box.innerHTML = data.map((item) => {
+    box.innerHTML = data.map((item) =>
         `
         <div class="product__card">
-            <h3 class="product__card_title">${item.title}</h3>
-            <p class="product__card_des">${item.description}</p>
+            <h3 class="product__card__title">${item.title}</h3>
+            <p class="product__card__des">${item.description}</p>
         </div>
         `
-    }).join("")
+    ).join("")
 }
 
 const getData = () => {
@@ -19,6 +21,40 @@ const getData = () => {
             render(data)
         })
 }
+
+input.addEventListener("keyup", (e) => {
+    const value = e.target.value;
+
+    if (value.length > 1) {
+        fetch(`http://localhost:3600/todos?title_like=${value}`)
+            .then((res) => res.json())
+            .then((data) => {
+                search_result.style.display = "block";
+                search_result.innerHTML = data.map((i) => `
+                <p class="search__cap">${i.title}</p>
+                `).join("")
+            })
+    } else {
+        search_result.style.display = "none";
+    }
+
+    console.log(value);
+})
+
+
+input.addEventListener("blur", (e) => {
+    search_result.style.display = "none";
+})
+
+input.addEventListener("focus", () => {
+    if (input.value.length > 2) {
+        search_result.style.display = "block"
+    } else {
+        search_result.style.display = "none";
+    }
+
+})
+
 
 
 
